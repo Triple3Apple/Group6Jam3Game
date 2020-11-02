083ddae17f3326d34b58799e275b5029d83f43e0
Abrar Akhand <Jahin345@gmail.com>
2020-11-01 18:40:04 -0700

    Pause menu added to game scene

--- Horror Game Jam Idea/Assets/Scripts/PauseMenu.cs
+++ Horror Game Jam Idea/Assets/Scripts/PauseMenu.cs
@@ -18,7 +18,8 @@ public class PauseMenu : MonoBehaviour
             {
                 Cursor.lockState = CursorLockMode.Locked;
                 Resume();
-            }else
+            }
+            else
             {
                 Cursor.lockState = CursorLockMode.None;
                 Pause();
@@ -26,14 +27,17 @@ public class PauseMenu : MonoBehaviour
         }
         
     }
+
     public void Resume ()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     void Pause ()
     {
+        Cursor.lockState = CursorLockMode.None;
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
